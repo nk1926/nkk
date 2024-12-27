f23728c26726d8b90d0e0ef02be588e3481d0133
nk1926 <nk1926907@gmail.com>
2024-12-27 15:33:54 +0300

    rename file

---
+++ test.c
@@ -0,0 +1,31 @@
+
+int one(int x)
+{
+    x = x + 8;
+    printf("\n XYZ = %d", x);
+    return (x);
+}
+
+float two(int m)
+{
+    one(m);
+    printf("\n AABC = %d", m);
+    return (7.1);
+}
+
+int three(int g, int h)
+{
+    int p, t;
+    float r;
+    g = one(g);
+    r = two(h);
+    printf("\n A = %d, B = %d, C = %f, D = %d", g, h, r, p);
+    return (25);
+}
+
+int main()
+{
+    int a = 11, b = 12, ht;
+    ht = three(14, 2);
+    printf("\n F = %d, G = %d, H = %d", a, b, ht);
+}
